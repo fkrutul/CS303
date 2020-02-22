6c468086683bcb44d288fc2a66cbe4c01c0af2cb
fkrutul <61340559+fkrutul@users.noreply.github.com>
2020-02-21 22:42:08 -0600

    Create BinaryTreeNode.java

---
+++ BinaryTreeNode.java
@@ -0,0 +1,16 @@
+
+package lab8;
+
+public class BinaryTreeNode {
+
+        long key;
+        BinaryTreeNode left, right;
+        String data;
+        
+        public BinaryTreeNode (long keyValue, String dataValue) {
+            key = keyValue;
+            data = dataValue;
+            left = null;
+            right = null;
+        }
+} 
